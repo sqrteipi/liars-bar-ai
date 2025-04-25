294b2bdb6d80c58ec361b46813b020d81ac2fb60
LazyEagleIsHere <lymivan1110@hotmail.com>
2025-04-25 14:29:55 +0800

    Small edit

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def game():
     debug_output = True
     
     # During Round
-    while alive.count(1) > 1:
+    while True:
         current_time = pygame.time.get_ticks()
         
         for event in pygame.event.get():
@@ -234,7 +234,6 @@ def game():
         ####################################################################################################
 
         else:
-            screen.fill("black")
             cur_player = round % 4
 
             debug_output = True
